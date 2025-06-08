23f9029ae6a122c19ccfe853b1f0828b9e8b9b76
Kavyansh Jain <69588134+KavyanshJain@users.noreply.github.com>
2025-06-08 13:51:20 +0000

    Created answer for assign 1 and readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CSI-Assignment-Week-1
 CSI assignment week 1
+
+## Problem Statement
+
+Create lower triangular, upper triangular and pyramid containing the "*" character.
+
+## Answer/Solution
+Use the main.py program in the repository.
+The script takes user input to get the height of the desired pyramid, then you can choose to print which kind of pyramid you want using the 'match' statement.
---
+++ main.py
@@ -0,0 +1,40 @@
+#code for printing different pyramids with their height as per user input
+
+#input for the height of the pyramid
+n = int(input("Enter the height of pyramid: "))
+
+t = int(input("1. Lower Pyramid\n2. Upper Pyramid\n3. Pyramid\n4.All\n Enter your pattern: "))
+
+match t:
+    case 1: #lower
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                print("*", end=" ")
+            print()
+    
+    case 2: #upper
+        for i in range(n, 0, -1):
+            print("*" * i, end=" \n")
+    
+    case 3: #normal
+        for i in range(1, n+1):
+            for j in range(n-i):
+                print(" ", end=" ")
+            for k in range(1, 2*i):
+                print("*", end=" ")
+            print()
+    case 4: #for all
+        for i in range(1, n+1): #lower
+            for j in range(1, i+1):
+                print("*", end=" ")
+            print()
+        for i in range(n, 0, -1): #upper
+            print("*" * i, end=" \n")
+        for i in range(1, n+1): #normal
+            for j in range(n-i):
+                print(" ", end=" ")
+            for k in range(1, 2*i):
+                print("*", end=" ")
+            print()
+    case _:
+        print("This is not a valid selection try again.")
